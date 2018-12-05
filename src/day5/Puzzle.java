196f2dbede045a612592d2217d04971741826a4a
cmgdev <cmgdev>
2018-12-05 13:24:45 -0600

    refactor for puzzle2

--- src/day5/Puzzle.java
+++ src/day5/Puzzle.java
@@ -3,44 +3,49 @@ package day5;
 import java.io.File;
 import java.nio.file.Files;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Puzzle {
 
     public static final String INPUT_FILE = System.getProperty("user.dir") + "/out/production/advent2018/day5/input.txt";
+    public static final boolean IS_TEST = true;
 
     public static void main(String[] args) {
-        try {
-            List<String> lines = Files.readAllLines(new File(INPUT_FILE).toPath());
+        String input = "dabAcCaCBAcCcaDA";
+        if (!IS_TEST) {
+            try {
+                input = Files.readAllLines(new File(INPUT_FILE).toPath()).get(0);
+            } catch (Exception e) {
+                System.out.println("Oh shit! " + e);
+            }
+        }
 
-            String input = lines.get(0);
+        String result = doPolymerReaction(input);
 
-            boolean changedThisIteration = true;
-            while (changedThisIteration) {
-                int startLength = input.length();
+        System.out.println("Final polymer: " + result);
+        System.out.println("Final length: " + result.length());  // 9526
+    }
 
-                for (int i = 0; i < input.length() - 1; i++) {
-                    Character thisChar = input.charAt(i);
-                    Character nextChar = input.charAt(i + 1);
-                    if (Character.toUpperCase(thisChar) == Character.toUpperCase(nextChar)
-                            && Character.isUpperCase(thisChar) != Character.isUpperCase(nextChar)) {
-                        System.out.println("Polymer reaction on: " + input);
-                        input = input.replaceFirst(new String(new char[]{thisChar, nextChar}), "");
-                        break;
-                    }
-                }
+    private static String doPolymerReaction(String input) {
+        boolean changedThisIteration = true;
+        do {
+            int startLength = input.length();
 
-                if (startLength == input.length()) {
-                    changedThisIteration = false;
+            for (int i = 0; i < input.length() - 1; i++) {
+                Character thisChar = input.charAt(i);
+                Character nextChar = input.charAt(i + 1);
+                if (Character.toUpperCase(thisChar) == Character.toUpperCase(nextChar)
+                        && Character.isUpperCase(thisChar) != Character.isUpperCase(nextChar)) {
+                    System.out.println("Polymer reaction on: " + input);
+                    input = input.replaceFirst(new String(new char[]{thisChar, nextChar}), "");
+                    break;
                 }
             }
 
-            System.out.println("Final polymer: " + input);
-            System.out.println("Final length: " + input.length());
-
-        } catch (Exception e) {
-            System.out.println("Oh shit! " + e);
-        }
+            if (startLength == input.length()) {
+                changedThisIteration = false;
+            }
+        } while (changedThisIteration);
+        return input;
     }
 }
