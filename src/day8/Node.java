62bf1fb93e37b701b10e2546230a1f1d54b297cf
cmgdev <cmgdev>
2018-12-08 15:04:16 -0600

    solved day8 puzzle1

---
+++ src/day8/Node.java
@@ -0,0 +1,48 @@
+package day8;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Node {
+
+    private List<Node> children = new ArrayList<>();
+    private List<Integer> metadata = new ArrayList<>();
+
+    public Node(Iterator<String> inputs) {
+        int numChildren = Integer.parseInt(inputs.next());
+        int numMetadatas = Integer.parseInt(inputs.next());
+
+        for (int i = 0; i < numChildren; i++) {
+            this.addChild(new Node(inputs));
+        }
+
+        for (int i = 0; i < numMetadatas; i++) {
+            this.addMetadata(Integer.parseInt(inputs.next()));
+        }
+    }
+
+    public void addChild(Node child) {
+        children.add(child);
+    }
+
+    public void addMetadata(Integer data) {
+        metadata.add(data);
+    }
+
+    public int getSumAllMetadata(){
+        int sum = getMetadataSum();
+        for( Node child: children ){
+            sum += child.getSumAllMetadata();
+        }
+        return sum;
+    }
+
+    public int getMetadataSum() {
+        return metadata.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+
+}
---
+++ src/day8/Puzzle.java
@@ -0,0 +1,38 @@
+package day8;
+
+import base.AbstractPuzzle;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Puzzle extends AbstractPuzzle {
+
+    public static final boolean IS_TEST = false;
+
+    public Puzzle() {
+        super(IS_TEST);
+    }
+
+    public static void main(String[] args) {
+        Puzzle puzzle = new Puzzle();
+        List<String> input = puzzle.readFile(8);
+
+        String[] inputs = input.get(0).split(" ");
+
+        Node tree = new Node(Arrays.asList(inputs).iterator());
+
+        /*************************
+         * Part 1
+         *************************/
+        int sumAllMetadata = tree.getSumAllMetadata();
+
+        System.out.println("Sum is " + sumAllMetadata);
+        if (IS_TEST) {
+            System.out.println(sumAllMetadata == 138);
+        } else {
+            System.out.println(sumAllMetadata == 49180);
+        }
+
+    }
+
+}
