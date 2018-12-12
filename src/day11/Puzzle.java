6fe79e8dc90c70af7f2dbab86d333a01c7de1b5d
cmgdev <cmgdev>
2018-12-11 18:54:00 -0600

    solved day11 puzzle2

--- src/day11/Puzzle.java
+++ src/day11/Puzzle.java
@@ -34,6 +34,42 @@ public class Puzzle {
             System.out.println("Failed!");
             return;
         }
+        System.out.println();
+
+        /******************
+         * Part 2
+         ******************/
+        int size = 0;
+        highestPower = 0;
+        x = 0;
+        y = 0;
+
+        int[][] grid = getFullGridPower(serialNumber);
+
+        for (int s = 1; s <= 300; s++) {
+            System.out.println("Starting size " + s);
+            for (int i = 1; i < 300 - s; i++) {
+                for (int j = 1; j < 300 - s; j++) {
+                    int gridPower = getSquareGridPower(grid, i, j, s, serialNumber);
+                    if (gridPower > highestPower) {
+                        highestPower = gridPower;
+                        x = i;
+                        y = j;
+                        size = s;
+                        System.out.println(highestPower + " @ " + x + "," + y + "," + size);
+                    }
+                }
+            }
+        }
+
+        String actual = highestPower + " @ " + x + "," + y + "," + size;
+        String expected = "79 @ 232,289,8";
+        System.out.println(actual);
+        if (!expected.equals(actual)) {
+            System.out.println("Failed!");
+        } else {
+            System.out.println("Passed!");
+        }
     }
 
     public static int get3x3GridPower(int startX, int startY, int serialNumber) {
@@ -46,6 +82,19 @@ public class Puzzle {
         for (int i = startX; i < startX + size; i++) {
             for (int j = startY; j < startY + size; j++) {
                 powerLevel += getPowerLevel(i, j, serialNumber);
+            }
+        }
+
+        return powerLevel;
+    }
+
+    public static int getSquareGridPower(int[][] grid, int startX, int startY, int size, int serialNumber) {
+        int powerLevel = 0;
+        int adjustedX = startX - 1;
+        int adjustedY = startY - 1;
+        for (int i = adjustedX; i < adjustedX + size; i++) {
+            for (int j = adjustedY; j < adjustedY + size; j++) {
+                powerLevel += grid[i][j];
             }
         }
 
@@ -67,5 +116,18 @@ public class Puzzle {
     public static int getPowerLevel(int x, int y, int serialNumber) {
         int rackId = x + 10;
         return (((((rackId * y) + serialNumber) * rackId) / 100) % 10) - 5;
+    }
+
+    public static int[][] getFullGridPower(int serialNumber) {
+        int size = 300;
+        int[][] grid = new int[size][size];
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                grid[i][j] = getPowerLevel(i + 1, j + 1, serialNumber);
+            }
+        }
+
+        return grid;
     }
 }
